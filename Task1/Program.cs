89db0d1c24ce6bbc271d6b599045bb9e7ee96675
AlamRishad <48613895+AlamRishad@users.noreply.github.com>
2021-09-28 23:25:35 +0600

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var name = GetName();
+            Console.WriteLine(name);
+        }
+
+        public static string GetName()
+        {
+            var a = GetObject();
+            String s = Convert.ToString((a));
+            string p = s.Substring(s.IndexOf("= ")+1, s.IndexOf(", ")-1);
+            string d = p.Substring(p.IndexOf(" ")+1, p.IndexOf(",")-1);
+            return d;
+
+        }
+
+        public static object GetObject()
+        {
+            return new { Name = "Camera", Price = 200 };
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var lines = GetLines();
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        public static string[] GetLines()
+        {
+            string[] line = new string[] { };
+            
+            string path=@"Tasks\Task3\DemoText.txt";
+            if(File.Exists(path))
+            {
+                line = File.ReadAllLines(path);
+                return line;
+            }
+            return line;
+        }
+    }
+    }
+
